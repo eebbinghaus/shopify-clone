850be436bdbdbc03b361f5948b42d9f40d4bca20
Erich <eebbinghaus@gmail.com>
2024-08-03 22:48:11 -0600

    added functionality to update and delete users

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -15,4 +15,29 @@ module.exports = {
             .then((user) => res.status(200).json(user))
             .catch((err) => res.status(500).json(err));
     },
+
+    //Update an existing user
+    updateUser(req, res) {
+        User.findOneAndUpdate({ _id: req.params.userId }, { $set: req.body })
+            .select("-_v")
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with this id!" })
+                    : res.json(user)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // Delete a user
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with that ID" })
+                    : res.json({ message: "User deleted!" })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 };
--- Server/routes/api/userRoutes.js
+++ Server/routes/api/userRoutes.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
-const { createUser, getUsers } = require("../../controllers/userController");
+const {
+    createUser,
+    getUsers,
+    updateUser,
+    deleteUser,
+} = require("../../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").put(updateUser).delete(deleteUser);
 
 module.exports = router;
